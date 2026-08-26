18310ca8bff84cdeadfb426f9b4ec8dcced8cd88
lizzdodizz12 <143068529+lizzdodizz12@users.noreply.github.com>
2026-08-26 23:53:05 +0800

    Fix Render Laravel 500 error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,6 @@ set -eu
 port="${PORT:-10000}"
 sed "s/__PORT__/${port}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+php artisan config:clear --no-interaction
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
