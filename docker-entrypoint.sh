2a8f12d53275cb9773d1b96f4b4939f082c069aa
lizzdodizz12 <143068529+lizzdodizz12@users.noreply.github.com>
2026-08-26 23:56:22 +0800

    Fix malformed Render application URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,11 @@ set -eu
 port="${PORT:-10000}"
 sed "s/__PORT__/${port}/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+case "${APP_URL:-}" in
+	http://*|https://*) ;;
+	*) export APP_URL="${RENDER_EXTERNAL_URL:-http://localhost}" ;;
+esac
+
 php artisan config:clear --no-interaction
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
