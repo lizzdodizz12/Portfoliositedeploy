1064bb5bc31f185510eec7157285635bcb5fa7f5
lizzdodizz12 <143068529+lizzdodizz12@users.noreply.github.com>
2026-09-03 00:21:17 +0800

    undo

--- config/contact.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'recipient' => env('CONTACT_EMAIL', 'romero.leechristian19@gmail.com'),
-];
--- resources/views/emails/contact-message.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>New portfolio contact message</h1>
-
-<p><strong>Name:</strong> {{ $contact['name'] }}</p>
-<p><strong>Email:</strong> {{ $contact['email'] }}</p>
-<p><strong>Subject:</strong> {{ $contact['subject'] }}</p>
-
-<p><strong>Message:</strong></p>
-<p>{!! nl2br(e($contact['message'])) !!}</p>
